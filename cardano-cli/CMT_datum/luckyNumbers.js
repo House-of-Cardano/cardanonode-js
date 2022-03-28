8d0955283bddd5c748b00701ec31c6de16e241a9
simonholmes001 <simonholmesabc@gmail.com>
2022-03-28 21:55:36 +0200

    Feature: can capture the lucky numbers as query parameters from the API URL and use this to feed the luckyNumbers.js and hashCMTDatum.js scripts which takes the lucky numbers and hashes them, fixes AB#405, fixes AB#411

--- cardano-cli/CMT_datum/luckyNumbers.js
+++ cardano-cli/CMT_datum/luckyNumbers.js
@@ -1,26 +1,43 @@
 const fs = require("fs");
 
+function getLuckyNumbers() {
+  const data = fs.readFileSync(
+    "../houseofcardano-explorer-testnet/data/datum",
+    "utf-8"
+  );
+  var jsonData = data;
+  var numbers = JSON.parse(jsonData);
+  const luckyNumbers = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    luckyNumbers.push(numbers[i]);
+  }
+  return luckyNumbers;
+}
+
+const luckyNumbers = getLuckyNumbers();
+
 const map = new Map();
 
-map.set("key1", 1);
-map.set("key2", 2);
-map.set("key3", 7);
-map.set("key4", 9);
-map.set("key5", 28);
+map.set("key1", luckyNumbers[0]);
+map.set("key2", luckyNumbers[1]);
+map.set("key3", luckyNumbers[2]);
+map.set("key4", luckyNumbers[3]);
+map.set("key5", luckyNumbers[4]);
 
 const datum = {
-    "constructor": 0,
-    "fields": [
-      {"bytes":"416363657074"},
-      {"int":map.get("key1")},
-      {"int":map.get("key2")},
-      {"int":map.get("key3")},
-      {"int":map.get("key4")},
-      {"int":map.get("key5")},
-    ],
-  };
+  constructor: 0,
+  fields: [
+    { bytes: "416363657074" },
+    { int: map.get("key1") },
+    { int: map.get("key2") },
+    { int: map.get("key3") },
+    { int: map.get("key4") },
+    { int: map.get("key5") },
+  ],
+};
 
-jsonDatum = JSON.stringify(datum);
+const jsonDatum = JSON.stringify(datum);
 
 fs.writeFile("./data/datum.json", jsonDatum, "utf8", function (err) {
 if (err) {
